dea14327030157b22f7943e7e20ba4f08a1cdaee
Goitseone-Themba <goitseonerozthemba@gmail.com>
2026-01-19 14:59:44 +0200

    probe tests and assertion library

---
+++ contracts/Counter.sol
@@ -0,0 +1,21 @@
+// SPDX-Licence-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+contract Counter {
+    uint public x;
+
+    event Increment(uint by);
+
+    function inc() public {
+        x += 1;
+        emit Increment(1);
+    }
+
+    function incBy(uint by) public {
+        require(by > 0, "incBy: increment should be positive");
+        x += by;
+        emit Increment(by);
+    }
+}
+
+// Random number to make the contract file unique: 207285923
